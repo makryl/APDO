1a3bd434206dc9f2e0f76275edcb0fcc3017ff82
Aequiternus <aequiternus@gmail.com>
2013-12-07 17:24:32 +0400

    exporter diff test

--- test/aeqdev/APDO/Schema-compare.sql
+++ test/aeqdev/APDO/Schema-compare.sql
@@ -5,16 +5,6 @@ CREATE TABLE apdo_test_tree (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE apdo_test_tree_extra (
-    id integer NOT NULL AUTO_INCREMENT,
-    heightttt integer,
-    tree_id integer NOT NULL,
-
-    FOREIGN KEY (tree_id) REFERENCES apdo_test_tree(id),
-    UNIQUE KEY (tree_id),
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE apdo_test_fruit (
     id integer NOT NULL AUTO_INCREMENT,
     name varchar(20) NOT NULL,
--- test/aeqdev/APDO/Schema-diff.sql
+++ test/aeqdev/APDO/Schema-diff.sql
@@ -1,12 +1,18 @@
--- ALTER TABLE apdo_test_tree_extra DROP heightttt;
-
 -- ALTER TABLE apdo_test_fruit DROP treeeee_id;
 
 -- DROP TABLE apdo_test_drop;
 
 -- ALTER TABLE apdo_test_tree MODIFY name varchar(20);
 
-ALTER TABLE apdo_test_tree_extra ADD height integer;
+CREATE TABLE apdo_test_tree_extra (
+    id integer NOT NULL AUTO_INCREMENT,
+    height integer,
+    tree_id integer NOT NULL,
+
+    FOREIGN KEY (tree_id) REFERENCES apdo_test_tree(id),
+    UNIQUE KEY (tree_id),
+    PRIMARY KEY (id)
+);
 
 ALTER TABLE apdo_test_fruit ADD tree_id integer;
 
